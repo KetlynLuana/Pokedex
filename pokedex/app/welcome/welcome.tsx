be1adc14e88f9ffc0aace221c754e695effc6d28
KetlynLuana <luanaketlyn80@gmail.com>
2026-03-26 10:09:56 -0300

    feat: :sparkles: Atividade do dia 26/03/26

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -6,9 +6,14 @@ export function Welcome() {
 
 
   return (
-    <main >
-       <h1>OI KETLYN!</h1>
-       <button className="border-2 border-red-600 bg-red-700 hover:cursor-pointer hover:bg-red-300" onClick={() => console.log ("oi")}>aperte para falar oi</button>
+    <main className="bg-red-500" >
+       <h1 className="text-2x1 ...">Pokedex</h1>
+    <h1 className="front-mono ..."></h1>
+      <h1 className="text-amber-600">Home</h1>
+      <h1 className="text-amber-600">Pokémons</h1>
+      <h1 className="text-amber-600">Tipos</h1>
+      
+      
     </main>
   );
 }
